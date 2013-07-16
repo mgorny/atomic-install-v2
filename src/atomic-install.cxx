dee65671b954701d93761a5c4d502cea4668348e
Michał Górny <mgorny@gentoo.org>
2013-07-16 11:01:00 +0200

    Initiate the codebase.

---
+++ src/atomic-install.cxx
@@ -0,0 +1,14 @@
+/**
+ * atomic-install
+ * (c) 2013 Michał Górny
+ * Released under the 2-clause BSD license
+ */
+
+#ifdef HAVE_CONFIG_H
+#	include "config.h"
+#endif
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
